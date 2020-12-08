181b8ac361c52fb403c95273eed64bcb910bd04d
Gladwinnnn <gladwin1503@hotmail.com>
2020-12-08 15:46:04 +0800

    Update IsUnique.java

--- Arrays and Strings/IsUnique.java
+++ Arrays and Strings/IsUnique.java
@@ -1,3 +1,7 @@
+/*
+    Question 1.1 - Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? 
+*/
+
 import java.util.*;
 
 public class IsUnique{
