22d3cd51d0cb6b28b2204f6ba255d997db33f71b
Gladwinnnn <gladwin1503@hotmail.com>
2020-12-13 17:23:40 +0800

    code comments

--- Arrays and Strings/PalindromePermutation.java
+++ Arrays and Strings/PalindromePermutation.java
@@ -10,28 +10,28 @@ public class PalindromePermutation{
         }
     }
 
-    public static Hashtable<Character,Integer> putIntoHashtable(String input){
+    public static Hashtable<Character,Integer> putIntoHashtable(String input){      // function to put every character into a hash table
         Hashtable<Character,Integer> result = new Hashtable<>();
-        String newInput = input.replace(" ", "").toLowerCase();
+        String newInput = input.replace(" ", "").toLowerCase();                     // remove all whitespaces as well as make the whole string lowercase
 
         for (int i = 0; i < newInput.length(); i++){
-            if (result.containsKey(newInput.charAt(i))){
-                result.put(newInput.charAt(i), result.get(newInput.charAt(i)) + 1);
+            if (result.containsKey(newInput.charAt(i))){                            // if the hash table already contains the character
+                result.put(newInput.charAt(i), result.get(newInput.charAt(i)) + 1); // increment the value of the key
             } else {
-                result.put(newInput.charAt(i), 1);
+                result.put(newInput.charAt(i), 1);                                  // else just put the character into the hash value with an initial value of 1
             }
         }
         return result;
     }
 
     public static boolean checkOdd(Hashtable<Character,Integer> hashTable){
-        boolean oneOdd = false;
-        for (char character: hashTable.keySet()){
-            if (hashTable.get(character) % 2 != 0){
-                if (oneOdd){
+        boolean oneOdd = false;                                                     // initialize this boolean to be false;
+        for (char character: hashTable.keySet()){                                   
+            if (hashTable.get(character) % 2 != 0){                                 // this is to check there is a key with value that is an odd numvber. There should only be a key value that is odd
+                if (oneOdd){                                                        // if this condition has already been fulfilled, return false as there can only be one
                     return false;
                 }
-                oneOdd = true;
+                oneOdd = true;                                                      // set this condition to be true to indicate conditon has already been fulfilled
             }
         }
         return true;
