f14e45c76c23cfef74e067146cfd541d150372e2
Gladwinnnn <gladwin1503@hotmail.com>
2020-12-15 21:20:46 +0800

    Update DeleteMiddleNode.java

--- Linked Lists/DeleteMiddleNode.java
+++ Linked Lists/DeleteMiddleNode.java
@@ -20,7 +20,7 @@ public class DeleteMiddleNode {
         }
     }
 
-    public static ArrayList<Integer> arrayList(Node node) {
+    public static ArrayList<Integer> arrayList(Node node) {     // creates an arrayList based on the nodes data
         ArrayList<Integer> resultArrayList = new ArrayList<>();
 
         while (node.next != null) {
@@ -31,13 +31,13 @@ public class DeleteMiddleNode {
         return resultArrayList;
     }
 
-    public static ArrayList<Integer> newArrayList(ArrayList<Integer> arrayList, int position) {
+    public static ArrayList<Integer> newArrayList(ArrayList<Integer> arrayList, int position) { // removing the element in a particular position in an arrayList
         ArrayList<Integer> resultArrayList = arrayList;
         arrayList.remove(position);
         return resultArrayList;
     }
 
-    public static int[] array(ArrayList<Integer> arrayList) {
+    public static int[] array(ArrayList<Integer> arrayList) {   // putting the elements in an arrayList into an array
         int[] resultArray = new int[arrayList.size()];
         
         for (int i = 0; i < arrayList.size(); i++) {
